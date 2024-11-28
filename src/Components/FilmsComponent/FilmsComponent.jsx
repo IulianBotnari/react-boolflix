8c6f311359823fd53d1e4c18cb5327b4784cd13b
IulianBotnari <iulianbotnari93@gmail.com>
2024-11-28 15:21:47 +0100

    Add layer on img

--- src/Components/FilmsComponent/FilmsComponent.jsx
+++ src/Components/FilmsComponent/FilmsComponent.jsx
@@ -14,15 +14,20 @@ export default function FilmsComponent() {
 
             <h1>Films</h1>
             {getFilms.results?.map((film, index) => (
-                <div key={index}>
+                <div className="relative w_185" key={index}>
 
-
-                    <p><strong>Titolo: </strong>{film.title}</p>
-                    <p><strong>Titolo originale: </strong>{film.original_title}</p>
-                    <p><strong>Lingua: </strong>{<Flag countryCode={film.original_language} svg />}</p>
-                    <p><strong>Descrizione: </strong>{film.overview}</p>
-                    <p><strong>Voto: </strong>{film.vote_average}</p>
                     <img src={`http://image.tmdb.org/t/p/w185${film.poster_path}`} />
+                    <div className="layer">
+                        <div className="absolute clolor">
+                            <p className="color "><strong>Titolo: </strong>{film.title}</p>
+                            <p className="color "><strong>Titolo originale: </strong>{film.original_title}</p>
+                            <p className="color "><strong>Lingua: </strong>{<Flag countryCode={film.original_language} svg />}</p>
+                            <p className="color "><strong>Descrizione: </strong>{film.overview}</p>
+                            <p className="color "><strong>Voto: </strong>{film.vote_average}</p>
+
+                        </div>
+
+                    </div>
 
                 </div>
             ))}
